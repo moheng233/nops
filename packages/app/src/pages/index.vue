822dcfb1293f20094f5b490fb856311b0133116b
moheng2333 <ylm200322400@163.com>
2024-03-01 10:24:32 +0800

    日常型更新 增加PWA配置,尝试窗口化的UI设计

--- packages/app/src/pages/index.vue
+++ packages/app/src/pages/index.vue
@@ -1,9 +1,110 @@
 <template>
-    <a>2</a>
+    <div class="dock-demo">
+        <div class="dock-window dock-advanced">
+            <Menubar style="height: 3vh;">
+                <template #start>
+                    <i class="pi pi-apple px-2"></i>
+                </template>
+                <template #end>
+                    <i class="pi pi-video px-2" />
+                    <i class="pi pi-wifi px-2" />
+                    <i class="pi pi-volume-up px-2" />
+                    <span class="px-2">{{ d(Date.now()) }}</span>
+                    <i class="pi pi-search px-2" />
+                    <i class="pi pi-bars px-2" />
+                </template>
+            </Menubar>
+
+            <Dock :model="docks">
+                <template #item="{ item }">
+                    <div v-tooltip="item.label" href="#" class="p-dock-link" >
+                        <img :src="item.icon" style="width: 100%;" />
+                    </div>
+                </template>
+            </Dock>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import vTooltip from "primevue/tooltip";
+import { useTimeAgo } from "@vueuse/core";
 
-const a  = ref(1);
+const { t, d } = useI18n();
+
+
+const docks = ref([
+    {
+        label: 'Finder',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/finder.svg",
+        command: () => {
+            
+        }
+    },
+    {
+        label: 'Terminal',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/terminal.svg",
+        command: () => {
+            
+        }
+    },
+    {
+        label: 'App Store',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/appstore.svg",
+        command: () => {
+            
+        }
+    },
+    {
+        label: 'Safari',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/safari.svg",
+        command: () => {
+            
+        }
+    },
+    {
+        label: 'Photos',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/photos.svg",
+        command: () => {
+            
+        }
+    },
+    {
+        label: 'GitHub',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/github.svg",
+    },
+    {
+        label: 'Trash',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/trash.png",
+        command: () => {
+            
+        }
+    }
+]);
 
 </script>
+
+<style>
+html,
+body {
+    padding: 0;
+    margin: 0;
+}
+</style>
+
+<style scoped>
+.dock-demo>.dock-window {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url("https://primefaces.org/cdn/primevue/images/dock/window.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
+
+.dock-demo .p-menubar {
+    padding: 0;
+    border-radius: 0;
+}
+</style>
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,15 +1,16 @@
+import VueI18n from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 import typescript from 'rollup-plugin-typescript2';
 import AutoImport from "unplugin-auto-import/vite";
 import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import { VueRouterAutoImports } from 'unplugin-vue-router';
 import VueRouter from "unplugin-vue-router/vite";
-import VueI18n from "@intlify/unplugin-vue-i18n/vite";
 import { defineConfig } from 'vite';
-import VueDevtools from "vite-plugin-vue-devtools";
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from "path";
+import { VitePWA } from 'vite-plugin-pwa';
+import VueDevtools from "vite-plugin-vue-devtools";
 
 
 export default defineConfig({
@@ -17,6 +18,34 @@ export default defineConfig({
   cacheDir: "../../node_modules/.vite/app",
   esbuild: false,
   plugins: [
+    VitePWA({
+      injectRegister: "script-defer",
+      registerType: "autoUpdate",
+      manifest: {
+        name: "NOps",
+        short_name: "nops",
+        theme_color: "#ffffff",
+        display_override: ["window-controls-overlay", "fullscreen", "minimal-ui"],
+        display: "standalone",
+        icons: [
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    }),
     viteStaticCopy({
       targets: [
         {
